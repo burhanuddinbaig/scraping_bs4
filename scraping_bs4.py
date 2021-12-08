72d9a1675e6dd48d24efdc93600513ae7812df9c
Burhanuddin <burhan.uddin.baig1@gmail.com>
2021-12-08 16:02:07 +0500

    first commit with funtional code

---
+++ scraping_bs4.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+
+html_doc = """<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+#print(soup.prettify())
+
+#s = soup.title.name
+
+#s = soup.title.string
+
+#s = soup.title.parent.name
+
+#s = soup.p	# to get a div
+
+s = soup.p['class']
+
+print(s)
